bf60182e0b4a7b2fd21aba453bb496d130c21038
Kevin <hill.kevin@earthlink.net>
2020-09-08 10:33:20 -0400

    tweaks to jt scoring algorithm to take into account strong answers, pass score.

--- jtTest.js
+++ jtTest.js
@@ -4,39 +4,46 @@ module.exports = function(){
 
 /*
 *   variable: key_string
-* additive value:   |  1  | 
-* -------------------------
-* I answer key:     |  0  |
-* E answer key:     |  1  |
-* S answer key:     |  2  |
-* N answer key:     |  3  |
-* T answer key:     |  4  |
-* F answer key:     |  5  |
-* J answer key:     |  6  |
-* P answer key:     |  7  |
+* additive value:   |  1  |  2  |
+* -------------------------------
+* I answer key:     |  0  |  A  |
+* E answer key:     |  1  |  B  |
+* S answer key:     |  2  |  C  |
+* N answer key:     |  3  |  D  |
+* T answer key:     |  4  |  E  |
+* F answer key:     |  5  |  F  |
+* J answer key:     |  6  |  G  |
+* P answer key:     |  7  |  H  |
 *
 */
 
 //jt calculation
 function calculate_jt(body){
     return new Promise(function(resolve,reject) {
+
+        //determine index for 2 point additive keys
+        function determine_scale(char_value){
+            var idx = char_value - 65;
+            return idx;
+        };
         const NUM_OPTIONS = 4;
         var key = body.keys_string;
         var key_length = key.length; 
-        var max = 0;
-        for (var j = 0; j < key_length; j++){
-            if(key[j] > max){
-            max = key[j];
-            }
-        }
-        max++;
-        var arr = Array.from({ length: max });
-        arr.fill(0);
+        var arr_length = 8;
+        var pref_arr = Array.from({ length: arr_length });
+        pref_arr.fill(0);
         ans_idx = 0;
         question_num = 0;
         for (var i = 0; i < key_length; i++){
             if (parseInt(body[ans_idx]) === (i - (question_num * NUM_OPTIONS))){
-            arr[key[i]]++;
+                //if key is integer 1 point allocation.
+                if (Number.isInteger(key[i])){
+                    pref_arr[key[i]]++;
+                }
+                else{
+                   var idx = determine_scale(key[i].charCodeAt());
+                   pref_arr[idx] += 2;
+                }
             }
             if ((i + 1) % NUM_OPTIONS == 0){
             question_num++;
@@ -47,18 +54,18 @@ function calculate_jt(body){
         //Determine jt based on additive scoring, preference given to more common
         var result_jt = '';
         var score = '';
-        var IE_total = arr[0] + arr[1];
-        var SN_total = arr[2] + arr[3];
-        var TF_total = arr[4] + arr[5];
-        var JP_total = arr[6] + arr[7];
-        var I_val = arr[0]/IE_total; 
-        var E_val = arr[1]/IE_total;
-        var S_val = arr[2]/SN_total; 
-        var N_val = arr[3]/SN_total;
-        var T_val = arr[4]/TF_total; 
-        var F_val = arr[5]/TF_total;
-        var J_val = arr[6]/JP_total; 
-        var P_val = arr[7]/JP_total;
+        var IE_total = pref_arr[0] + pref_arr[1];
+        var SN_total = pref_arr[2] + pref_arr[3];
+        var TF_total = pref_arr[4] + pref_arr[5];
+        var JP_total = pref_arr[6] + pref_arr[7];
+        var I_val = pref_arr[0]/IE_total; 
+        var E_val = pref_arr[1]/IE_total;
+        var S_val = pref_arr[2]/SN_total; 
+        var N_val = pref_arr[3]/SN_total;
+        var T_val = pref_arr[4]/TF_total; 
+        var F_val = pref_arr[5]/TF_total;
+        var J_val = pref_arr[6]/JP_total; 
+        var P_val = pref_arr[7]/JP_total;
         if (E_val > I_val){
             result_jt += 'E';
             score += 'E:';
@@ -125,8 +132,7 @@ function calculate_jt(body){
         result_arr[0] = {};
         result_arr[1] = {};
         result_arr[0].result = result_jt;
-        console.log(score);
-        result_arr[1] = 'I:1,E:2'; 
+        result_arr[1] = score; 
         resolve(result_arr);
     });
 }
